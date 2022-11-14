9c7bd098f61f77dc2ddd982f91f8b9626ec2050f
Ryan Song <rs2122@ic.ac.uk>
2022-11-14 13:35:09 +0000

    Extensions not finished

--- src/main/kotlin/webserver/WebServer.kt
+++ src/main/kotlin/webserver/WebServer.kt
@@ -3,21 +3,63 @@ package webserver
 // write your web framework code here:
 
 fun scheme(url: String): String = url.substringBefore(":")
-
-fun host(url: String): String = url.substringAfter("://").substringBefore("/")
-
-fun path(url: String): String = url.substringAfter(host(url)).substringBefore("?")
+fun host(url: String): String   = url.substringAfter("://").substringBefore("/")
+fun path(url: String): String   = url.substringAfter(host(url)).substringBefore("?")
 
 fun queryParams(url: String): List<Pair<String, String>> {
-  val allParams = url.substringAfter("?")
-  return emptyList()
+  return if (url.substringAfter(host(url)) == "") {
+    emptyList()
+  } else {
+    val allParams = url.substringAfter("?").split("&")
+
+    allParams.map { x -> x.split("=") }.map { y -> y.zipWithNext() }.flatten()
+  }
 }
 
 // http handlers for a particular website...
 
-fun homePageHandler(request: Request): Response = Response(Status.OK, "This is Imperial.")
+fun route(request: Request): Response {
+  val url = request.url
+  return when (path(url)) {
+      "/" -> homePageHandler(request)
+      "/say-hello" -> helloHandler(request)
+      "/computing" -> computingPageHandler(request)
+      "/search" -> searchPageHandler(request)
+      else -> errorHandler(request)
+  }
+}
+fun homePageHandler(request: Request): Response      = Response(Status.OK, "This is Imperial.")
+fun computingPageHandler(request: Request): Response = Response(Status.OK, "This is DoC.")
+fun errorHandler(request: Request): Response         = Response(Status.NOT_FOUND, "404: PAGE NOT FOUND")
+fun searchPageHandler(request: Request): Response    = Response(Status.OK, "What would you like to know about ICL?")
+
+fun helloHandler(request: Request): Response {
+  val url       = request.url
+  val nameParam = queryParams(url).filter { t -> t.first == "name" }
+  val style     = queryParams(url).filter { t -> t.first == "style" }
+
+  // if there is a query parameter called "name"
+  return if (nameParam.isEmpty()) {
+    when {
+      style.isEmpty()                      -> Response(Status.OK, "Hello, World!")
+      style.first().second == "shouting"   -> Response(Status.OK, "HELLO, WORLD!")
+      style.first().second == "whispering" -> Response(Status.OK, "hello, world")
+      else                                 -> Response(Status.OK, "Invalid Response")
+    }
+  } else {
+    val name      = nameParam.first().second
+    val nameLower = name.lowercase()
+    val nameUpper = name.uppercase()
+    when {
+      style.isEmpty()                      -> Response(Status.OK, "Hello, $name!")
+      style.first().second == "shouting"   -> Response(Status.OK, "HELLO, $nameUpper!")
+      style.first().second == "whispering" -> Response(Status.OK, "hello, $nameLower")
+      else                                 -> Response(Status.OK, "Invalid Response")
+    }
+  }
+}
+
+
 fun main() {
-  println(scheme("https://www.google.com/search?q=kotlin&safe=active"))
-  println(host("https://www.google.com/search?q=kotlin&safe=active"))
-  println(path("https://www.google.com/search?q=kotlin&safe=active"))
+  println(path("http://www.imperial.ac.uk/"))
 }
